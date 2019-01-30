ba63fda1107d0b1d3372a1a072d2ef2c473a649f
hakver29 <haakongv@stud.ntnu.no>
2019-01-30 13:22:44 +0100

    modify main.py

--- game/main.py
+++ game/main.py
@@ -1,3 +1,26 @@
-if __name__ == '__main__':
-    print('Hello world')
-    print('Good bye!')
+import pygame
+
+# Initializes the game
+x = pygame.init()
+print(x)
+
+gameDisplay = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Experts in Team")
+
+# pygame.display.flip()
+
+pygame.display.update()
+
+gameExit = False
+
+# Prevents the game from shutting down
+# event is a log of what has happened
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+
+# Exits the game
+pygame.quit()
+quit()
