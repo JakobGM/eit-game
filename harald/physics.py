a57647c686ec1e882a845e8c0904940a15bcae07
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-02-06 10:29:31 +0100

    physics: Add Physics.move_players

--- harald/physics.py
+++ harald/physics.py
@@ -9,3 +9,9 @@ class Physics:
     def __init__(self, arena: arena.Arena, players: List[agent.Player]):
         self.arena = arena
         self.players = players
+
+    def move_players(self) -> None:
+        for player in self.players:
+            force = player.force
+            position = player.position
+            position[:] += force
--- harald/tests/test_physics.py
+++ harald/tests/test_physics.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from harald.physics import Physics
 from harald.arena import Arena
 from harald.agent import Player
@@ -8,3 +10,35 @@ def test_invokation_of_physics_object():
     players = [Player(), Player()]
     arena = Arena()
     physics = Physics(arena=arena, players=players)
+
+
+def test_update_players():
+    """Test movement of players based on player force."""
+    player = Player()
+
+    # Downwards force
+    player.force = np.array([0, -1])
+
+    arena = Arena()
+    physics = Physics(arena=arena, players=[player])
+
+    # Originally located at the origin
+    np.testing.assert_array_equal(
+        player.position,
+        np.array([0, 0]),
+    )
+
+    # Move one step down
+    physics.move_players()
+    np.testing.assert_array_equal(
+        player.position,
+        np.array([0, -1]),
+    )
+
+    # Change force towards right
+    player.force = np.array([1, 0])
+    physics.move_players()
+    np.testing.assert_array_equal(
+        player.position,
+        np.array([1, -1]),
+    )
