d05af0462e71eeb5d9a668d8979c54bf529c35fa
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-02-06 14:03:51 +0100

    physics: Add scaffolding for arena layer

--- harald/arena.py
+++ harald/arena.py
@@ -1,2 +1,39 @@
+import abc
+
+import numpy as np
+from numpy.linalg import norm
+
+from harald.agent import Player
+
+
 class Arena:
-    pass
+    def __init__(layers: List[ArenaLayer]) -> None:
+        self.layers = layers
+
+    def force(player):
+        total_force = np.zeros(2, dtype=float)
+        for layer in layers:
+            total_force += layer.force(player=player)
+        return total_force
+
+
+class ArenaLayer(abc.ABC):
+    @abc.abstractmethod
+    def force(self, player: Player) -> np.array:
+        """Return force for specific player."""
+
+
+class FrictionLayer(ArenaLayer):
+    def __init__(self, friction_matrix: np.ndarray) -> None:
+        self.friction = friction_matrix
+
+    def force(self, player: Player) -> np.array:
+        velocity = player.velocity
+        if norm(velocity) == 0:
+            return np.array([0., 0.])
+
+        position = player.position
+        x, y = position.tolist()
+        mu = self.friction[int(x), int(y)]
+        force = -mu * velocity / norm(velocity)
+        return force
