0aa38a7223c8fcb9ecf2f3968fb7397180f51dbe
Yanzhe Bekkemoen <yanzheb@stud.ntnu.no>
2019-02-20 09:33:45 +0100

    Add input

---
+++ haakon/input.py
@@ -0,0 +1,29 @@
+from typing import List, Tuple, Dict
+import numpy as np
+import pygame as pg
+
+class Input:
+    def __init__(self, keys: Dict[str, int]):
+        self.keys: Dict[str, int] = keys
+
+    def get_move(self):
+        keys = Input.get_key_pressed()
+        move = np.zeroes(2)
+
+        if keys[self.keys['left']]:
+            move += np.array([-1, 0])
+
+        if keys[self.keys['right']]:
+            move += np.array([1, 0])
+
+        if keys[self.keys['up']]:
+            move += np.array([0, 1])
+
+        if keys[self.keys['down']]:
+            move += np.array([0, -1])
+
+        return move
+
+    @staticmethod
+    def get_key_pressed() -> Tuple[int]:
+        return pg.key.get_pressed()
