5dd13db4cc48ce143e491d7c3de5c6319554db62
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-03-26 18:03:37 +0100

    stats: Use save_path fixture to remove clutter

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,9 +1,18 @@
 from pathlib import Path
 
+import pytest
+
 from modules.statistics import Statistics
 
-def test_creation_of_save_file():
+
+@pytest.fixture
+def save_path(tmpdir):
+    """Return a temporary save file path."""
+    return Path(tmpdir) / 'savefile'
+
+
+def test_creation_of_save_file(save_path):
     """Statistics object should automatically create save file."""
-    stats = Statistics()
+    stats = Statistics(save_path = save_path)
     save_path = stats.save_path
     assert save_path.exists()
