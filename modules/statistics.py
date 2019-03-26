4f580097b86ca5401c828d96751d2a3a8eadf08e
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-03-26 18:45:53 +0100

    stats: Implement data class for pickling

--- modules/statistics.py
+++ modules/statistics.py
@@ -7,10 +7,25 @@ for the analyzis of this data, such as the scoreboard, player rankings and so
 on.
 """
 
+import pickle
 from pathlib import Path
+from typing import List, Optional
 
 
 DEFAULT_SAVE_PATH = Path(__file__).parents[1] / 'data' / 'savefile'
+
+
+class Data:
+    """Class responsible for storing data related to the statistics module."""
+
+    def __init__(self):
+        """
+        Construct Data object.
+
+        No parameters are accepted, as the class is intended to be instantiated
+        with no data and then mutated with new data that may come in.
+        """
+        self.all_rankings = []
 
 
 class Statistics:
@@ -21,30 +36,51 @@ class Statistics:
     API.
     """
 
-    def __init__(self, save_path: Path = DEFAULT_SAVE_PATH) -> bool:
+    def __init__(self, save_path: Path = DEFAULT_SAVE_PATH) -> None:
         """
         Construct statistics object from save file stored on disk.
 
         :param save_path: Path to file that contains saved games.
         """
+        assert isinstance(save_path, Path)
         self.save_path = save_path
-        self.load_save_file(save_path)
+        self.load_data()
 
-    @staticmethod
-    def load_save_file(path: Path) -> bool:
+    def save(self, ranking: Optional[List[str]] = None) -> None:
+        """
+        Save new data from a game for later retrieval and analysis.
+
+        :param ranking: A list of strings ordered by whom did best during the
+          game in descending order.
+          For instance ranking=['gold', 'silver', 'bronze'].
+        """
+        if ranking:
+            assert isinstance(ranking, list)
+            assert isinstance(ranking[0], str)
+            self.data.all_rankings.append(ranking)
+        self.save_data()
+
+    def load_data(self) -> None:
         """
         Load the save data stored in the given path.
 
         If the path does not exist, the save file is created.
+        The data is persisted to self.data.
 
         :param path: Absolute path to savefile.
-        :return: Returns True if the savefile already exists, False otherwise.
         """
-        assert path.is_absolute()
-        path.parent.mkdir(parents=True, exist_ok=True)
+        assert self.save_path.is_absolute()
+        self.save_path.parent.mkdir(parents=True, exist_ok=True)
 
-        if not path.exists():
-            path.touch()
-            return False
+        if not self.save_path.exists():
+            self.save_path.touch()
+            self.data = Data()
+            self.save_data()
         else:
-            return True
+            self.data = pickle.loads(self.save_path.read_bytes())
+
+    def save_data(self) -> None:
+        """Save data to path specified by __init__ save_path parameter."""
+        self.save_path.write_bytes(
+            data=pickle.dumps(self.data, protocol=pickle.HIGHEST_PROTOCOL),
+        )
